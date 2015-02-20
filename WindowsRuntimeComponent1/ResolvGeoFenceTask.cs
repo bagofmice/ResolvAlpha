3c25982d34e406761acb325bb6a340443c19c5c4
bagofmice <bagofmice@gmail.com>
2015-02-20 03:53:27 -0800

    File dump for alpha
    Just a file dump, not a proper repository

---
+++ WindowsRuntimeComponent1/ResolvGeoFenceTask.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Geolocation;
+using Windows.Devices.Geolocation.Geofencing;
+using Windows.UI.Notifications;
+
+namespace Resolv
+{
+
+    public static class ResolvFenceHelper
+    {
+        public static IList<Geofence> GetGeofences()
+        {
+            return GeofenceMonitor.Current.Geofences;
+        }
+
+        public static IList<Geofence> GetResolvGeofences()
+        {
+            List<Geofence> resolvGeofences = new List<Geofence>();
+            foreach (Geofence g in GeofenceMonitor.Current.Geofences)
+            {
+                if (g.Id.StartsWith("resolv"))
+                {
+                    resolvGeofences.Add(g);
+                }
+            }
+
+            return resolvGeofences;
+        }
+
+        public static void CreateGeofence(string id, double lat, double lon, double radius, TimeSpan dwelltime)
+        {
+            if (GeofenceMonitor.Current.Geofences.SingleOrDefault(g => g.Id == id) != null) return;
+
+            BasicGeoposition position = new BasicGeoposition();
+            position.Latitude = lat;
+            position.Longitude = lon;
+
+            Geocircle geocircle = new Geocircle(position, radius);
+
+            MonitoredGeofenceStates mask = 0;
+            mask |= MonitoredGeofenceStates.Entered;
+
+            // Create Geofence with the supplied id, geocircle and mask, not for single use
+            // and with a dwell time of 5 seconds
+            Geofence geofence = new Geofence(id, geocircle, mask, false, dwelltime);
+            GeofenceMonitor.Current.Geofences.Add(geofence);
+        }
+
+        public static void RemoveGeofence(string id)
+        {
+            var geofence = GeofenceMonitor.Current.Geofences.SingleOrDefault(g => g.Id == id);
+
+            if (geofence != null)
+                GeofenceMonitor.Current.Geofences.Remove(geofence);
+        }
+    }
+
+    class ResolvGeoFenceTask : IBackgroundTask
+    {
+
+        // Task Standard Registration
+        const string ResolvTaskName = "ResolvFenceTask";
+        /// <summary>
+        /// Don't re-register
+        /// </summary>
+        /// <returns>If it's registered.</returns>
+        public static bool ISResolvBackTaskRegistered()
+        {
+            
+            var resolvTask =
+                BackgroundTaskRegistration.AllTasks.FirstOrDefault(kvp => kvp.Value.Name.Equals(ResolvTaskName));
+
+            if (resolvTask.Value != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async static void RegisterResolvBackgroundTask()
+        {
+            if (!ISResolvBackTaskRegistered())
+            {
+                object waitFor = await BackgroundExecutionManager.RequestAccessAsync();
+                BackgroundTaskBuilder resolvBuilder = new BackgroundTaskBuilder();
+                resolvBuilder.Name = ResolvTaskName;
+                resolvBuilder.TaskEntryPoint = typeof (ResolvGeoFenceTask).FullName;
+                resolvBuilder.SetTrigger(new LocationTrigger(LocationTriggerType.Geofence));
+                resolvBuilder.Register();
+            }
+        }
+
+        /// <summary>
+        /// Where the action is.
+        /// </summary>
+        /// <param name="taskInstance"></param>
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Get the information of the geofence(s) that have been hit
+            var reports = GeofenceMonitor.Current.ReadReports();
+            var report = reports.FirstOrDefault(r => (r.Geofence.Id == "MyGeofenceId") && (r.NewState == GeofenceState.Entered));
+
+            if (report == null) return;
+
+            // Create a toast notification to show a geofence has been hit
+            var toastXmlContent = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+            var txtNodes = toastXmlContent.GetElementsByTagName("text");
+            txtNodes[0].AppendChild(toastXmlContent.CreateTextNode("Geofence triggered toast!"));
+            txtNodes[1].AppendChild(toastXmlContent.CreateTextNode(report.Geofence.Id));
+
+            var toast = new ToastNotification(toastXmlContent);
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            toastNotifier.Show(toast);
+        }
+
+
+    }
+}
---
+++ WindowsRuntimeComponent1/YelpHelper.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CommandLine;
+using Newtonsoft.Json.Linq;
+using SimpleOAuth;
+using System.Net;
+//using System.Web;
+
+
+namespace Resolv
+{
+
+
+/// <summary>
+/// Yelp API v2.0 code sample.
+/// This program demonstrates the capability of the Yelp API version 2.0
+/// by using the Search API to query for businesses by a search term and location,
+/// and the Business API to query additional information about the top result
+/// from the search query.
+///
+/// Please refer to http://www.yelp.com/developers/documentation for the API documentation.
+///
+/// Sample usage of the program:
+/// `YelpAPI.exe --term="bars" --location="San Francisco, CA"`
+/// </summary>
+
+    /// <summary>
+    /// Class that encapsulates the logic for querying the API.
+    ///
+    /// Users have to set the OAuth credentials properties
+    /// in order to use this class.
+    /// </summary>
+    class YelpAPIClient
+    {
+        /// <summary>
+        /// Consumer key used for OAuth authentication.
+        /// This must be set by the user.
+        /// </summary>
+        private const string CONSUMER_KEY = "vrBPbgyElMdf4uBwDgXaxQ";
+
+        /// <summary>
+        /// Consumer secret used for OAuth authentication.
+        /// This must be set by the user.
+        /// </summary>
+        private const string CONSUMER_SECRET = "_9JctR_ZLejZtD8sTFhXwF0YM_c";
+
+        /// <summary>
+        /// Token used for OAuth authentication.
+        /// This must be set by the user.
+        /// </summary>
+        private const string TOKEN = "VAJ0bEn7JET3GSk62RheNR6VVQcjX-dN";
+
+        /// <summary>
+        /// Token secret used for OAuth authentication.
+        /// This must be set by the user.
+        /// </summary>
+        private const string TOKEN_SECRET = "40bAjHWc7omWQuDAGTnnAOaDFsc";
+
+        /// <summary>
+        /// Host of the API.
+        /// </summary>
+        private const string API_HOST = "http://api.yelp.com";
+
+        /// <summary>
+        /// Relative path for the Search API.
+        /// </summary>
+        private const string SEARCH_PATH = "/v2/search/";
+
+        /// <summary>
+        /// Relative path for the Business API.
+        /// </summary>
+        private const string BUSINESS_PATH = "/v2/business/";
+
+        /// <summary>
+        /// Search limit that dictates the number of businesses returned.
+        /// </summary>
+        private const int SEARCH_LIMIT = 10;
+
+        /// <summary>
+        /// Prepares OAuth authentication and sends the request to the API.
+        /// </summary>
+        /// <param name="baseURL">The base URL of the API.</param>
+        /// <param name="queryParams">The set of query parameters.</param>
+        /// <returns>The JSON response from the API.</returns>
+        /// <exception>Throws WebException if there is an error from the HTTP request.</exception>
+        private JObject PerformRequest(string baseURL, Dictionary<string, string> queryParams=null)
+        {
+            Dictionary<string, string> query = SimpleOAuth.Utilities.UrlHelper.ParseQueryString(String.Empty);
+                //System.Web.HttpUtility.ParseQueryString(String.Empty);
+            
+
+            if (queryParams == null)
+            {
+                queryParams = new Dictionary<string, string>();
+            }
+
+            foreach (var queryParam in queryParams)
+            {
+                query[queryParam.Key] = queryParam.Value;
+            }
+
+            UriBuilder uriBuilder = new UriBuilder(baseURL);
+            uriBuilder.Query = query.ToString();
+
+            var request = HttpWebRequest.Create(uriBuilder.ToString());
+            request.Method = "GET";
+
+            request.SignRequest(
+                new Tokens {
+                    ConsumerKey = CONSUMER_KEY,
+                    ConsumerSecret = CONSUMER_SECRET,
+                    AccessToken = TOKEN,
+                    AccessTokenSecret = TOKEN_SECRET
+                }
+            ).WithEncryption(EncryptionMethod.HMACSHA1).InHeader();
+
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+            var stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            return JObject.Parse(stream.ReadToEnd());
+        }
+
+        /// <summary>
+        /// Query the Search API by a search term and location.
+        /// </summary>
+        /// <param name="term">The search term passed to the API.</param>
+        /// <param name="location">The search location passed to the API.</param>
+        /// <returns>The JSON response from the API.</returns>
+        public JObject Search(string term, string location)
+        {
+            string baseURL = API_HOST + SEARCH_PATH;
+            var queryParams = new Dictionary<string, string>()
+            {
+                { "term", term },
+                { "location", location },
+                { "limit", SEARCH_LIMIT.ToString() }
+            };
+            return PerformRequest(baseURL, queryParams);
+        }
+
+        /// <summary>
+        /// Query the Business API by a business ID.
+        /// </summary>
+        /// <param name="business_id">The ID of the business to query.</param>
+        /// <returns>The JSON response from the API.</returns>
+        public JObject GetSearchData(string business_id)
+        {
+            string baseURL = API_HOST + SEARCH_PATH + business_id;
+            return PerformRequest(baseURL);
+        }
+    }
+}
+
